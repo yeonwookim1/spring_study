c9e92548bca3dbfa11550edb6d5c35d5b121a7a1
yeonwookim1 <kop4845@naver.com>
2022-06-14 22:45:34 +0900

    add singleton Test

---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,42 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        //ThreadA: A사용자가 10000원 주문
+        statefulService1.order("userA",10000);
+        //ThreadA: B사용자가 20000원 주문
+        statefulService2.order("userB",20000);
+
+        //ThreadA: 사용자A 주문금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+        System.out.println(System.identityHashCode(statefulService1));
+        System.out.println(System.identityHashCode(statefulService2));
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+
+    }
+
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+
+}
