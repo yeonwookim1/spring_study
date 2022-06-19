f12f6c4d1be2e13ddbbdb82d1743d6eac04d7e76
yeonwookim1 <kop4845@naver.com>
2022-06-19 20:37:18 +0900

    add component

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,16 @@
 package hello.core;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = {"hello.core", "hello.service"},
+//        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        //AppConfig configuration 중복 방지
+)
 public class AutoAppConfig {
-    
+
 }
