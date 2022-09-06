a76a95fcef1a510b373f5f51a34a997f3c9eafe3
yeonwookim1 <kop4845@naver.com>
2022-09-06 21:47:03 +0900

    add category

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -7,9 +7,18 @@ import javax.persistence.Embeddable;
 
 @Embeddable //내장형으로 사용하겠다.
 @Getter
-@Setter
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //값 타입은 변경 불가능한 클래스로 만듬
+    //임베디드 타입의 기본생성자는 public 또는 protected를 사용
+    protected Address(){
+    }
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
