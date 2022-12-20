a9e8fd468acdae61aafb1311b3678398d8700783
yeonwookim1 <kop4845@naver.com>
2022-12-20 21:57:30 +0900

    add jpa_flush

--- jpa/src/main/java/hello/JpaMain.java
+++ jpa/src/main/java/hello/JpaMain.java
@@ -21,40 +21,46 @@ public class JpaMain {
             Member member = new Member(12L, "kyw");
 
             //수정
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println(findMember.getName());
-            //persist를 하지 않아도 값이 변경된다.
-            findMember.setName("spirng_jpa");
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println(findMember.getName());
+//            //persist를 하지 않아도 값이 변경된다.
+//            findMember.setName("spirng_jpa");
 
             //jpql
             //객체를 대상으로 쿼리
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(5)
-//                    .setMaxResults(10)
-                    .getResultList();
-
-            for(Member m : resultList){
-                System.out.println(m.getName());
-            }
+//            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+////                    .setFirstResult(5)
+////                    .setMaxResults(10)
+//                    .getResultList();
+//
+//            for(Member m : resultList){
+//                System.out.println(m.getName());
+//            }
 
 
             //비영속
-            Member member1 = new Member(27L, "kyw11");
+//            Member member1 = new Member(27L, "kyw11");
+//
+//            //영속
+//            em.persist(member1);
 
-            //영속
-            em.persist(member1);
-
-            Member findMember1 = em.find(Member.class, 27L);
-            Member findMember2 = em.find(Member.class, 27L);
-            System.out.println("result = " + (findMember1 == findMember2)); //true
+//            Member findMember1 = em.find(Member.class, 27L);
+//            Member findMember2 = em.find(Member.class, 27L);
+//            System.out.println("result = " + (findMember1 == findMember2)); //true
 
             //commit
-            Member memberC1 = new Member(150L,"A");
-            Member memberC2 = new Member(151L,"B");
+//            Member memberC1 = new Member(150L,"A");
+//            Member memberC2 = new Member(151L,"B");
+//
+//            em.persist(memberC1);
+//            em.persist(memberC2);
 
-            em.persist(memberC1);
-            em.persist(memberC2);
+
+            Member fMember = new Member(200L,"FLUSH");
+            em.persist(fMember);
+            em.flush();
+            System.out.println("flush");
 
 
             tx.commit();
